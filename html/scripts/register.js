689bf01682279de4e1154b39c688e4579cbb4df3
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-11-28 22:42:20 +0200

    login and register started

---
+++ html/scripts/register.js
@@ -0,0 +1,50 @@
+const autoCompleteInputEl = document.querySelector('#autocomplete-input');
+// This is only mockup data!
+const kunnat = ["Helsinki","Vantaa","Espoo","Joensuu","Rovaniemi", "Hamina", "Elojärvi"];
+
+const onInputChange = () => {
+    removeAutocompleteDropdown();
+
+    const value = autoCompleteInputEl.value.toLowerCase();
+
+    if (value.length === 0) return;
+
+    const filteredKunnat = kunnat.filter((kunta) => {
+        return kunta.toLowerCase().startsWith(value);
+    });
+
+    createAutoCompleteDropDown(filteredKunnat)
+}
+
+const onKuntaButtonClick = (e) => {
+    e.preventDefault();
+    const buttonEl = e.target;
+    autoCompleteInputEl.value = buttonEl.innerHTML;
+
+    removeAutocompleteDropdown();
+}
+
+const createAutoCompleteDropDown = array => {
+    const listEl = document.createElement('ul');
+    listEl.className = 'autocomplete-list';
+    listEl.id = 'autocomplete-list';
+
+    array.forEach((kunta) => {
+        const listItem = document.createElement('li');
+        const kuntaButton = document.createElement('button');
+        kuntaButton.innerHTML = kunta;
+        kuntaButton.addEventListener('click', onKuntaButtonClick);
+        listItem.appendChild(kuntaButton);
+        listEl.appendChild(listItem);
+    });
+
+    document.querySelector('#autocomplete-wrapper').appendChild(listEl);
+}
+
+const removeAutocompleteDropdown = () => {
+    const listEl = document.querySelector('#autocomplete-list');
+
+    if (listEl) listEl.remove();
+}
+
+autoCompleteInputEl.addEventListener('input', onInputChange);
