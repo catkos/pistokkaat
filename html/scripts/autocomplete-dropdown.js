1e866ccb47398d85f064eb4091f4295df0e8cb47
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-12-11 17:55:33 +0200

    register and login done

--- html/scripts/autocomplete-dropdown.js
+++ html/scripts/autocomplete-dropdown.js
@@ -1,66 +1,61 @@
+'use strict';
+let selectedID;
+
 const onInputChange = (appendEl, input, array) => {
     removeAutocompleteDropdown();
-
     // Get value from input
     const value = input.value.toLowerCase();
-
     // If input's value's length is 0, create autocomplete drowdown with array
     if (value.length === 0) {
         createAutoCompleteDropDown(appendEl, array);
         return;
     }
-
     // Create list from array with values that start the same as input's value
     const filteredArray = array.filter((item) => {
-        return item.toLowerCase().startsWith(value);
+        return item.name.toLowerCase().startsWith(value);
     });
-
     createAutoCompleteDropDown(appendEl, filteredArray)
-}
+};
 
 const onButtonClick = (e, input) => {
     // Prevent default event
     e.preventDefault();
-
     // Get the button that is the target of the click event
     const buttonEl = e.target;
-
     // Add buttonEl's innerHTML to input's value
     input.value = buttonEl.innerHTML;
-
+    selectedID = buttonEl.value;
     removeAutocompleteDropdown();
-}
+};
 
 const createAutoCompleteDropDown = (appendEl, array) => {
     // Create ul for dropdown
     const listEl = document.createElement('ul');
     listEl.className = 'dropdownList';
     listEl.id = 'dropdownList';
-
     // Foreach array to button's inside list
     array.forEach((item) => {
         const listItem = document.createElement('li');
         const button = document.createElement('button');
-        button.innerHTML = item;
+        button.innerHTML = item.name;
+        button.value = item.id;
         button.addEventListener('click', (e) => {
             onButtonClick(e, dropdownInput);
         });
         listItem.appendChild(button);
         listEl.appendChild(listItem);
     });
-
     // Append ul list to dropdownWrapper
     appendEl.appendChild(listEl);
-}
+};
 
 const removeAutocompleteDropdown = () => {
     // Get ul list
     const listEl = document.querySelector('#dropdownList');
-
     // If it exists, remove it
     if (listEl) listEl.remove();
-}
+};
 
 const addFocusToInputEl = (input) => {
     input.focus();
-}
+};
--- html/scripts/register.js
+++ html/scripts/register.js
@@ -1,10 +1,80 @@
-// TODO: get municipalities from api
-// This is only mockup data!
-const municipalities = ["Helsinki","Vantaa","Espoo","Joensuu","Rovaniemi", "Hamina", "Elojärvi"];
-
+'use strict';
+// TODO: Change url when uploading to server
+const url = 'http://localhost:3000';
+let municipalities = [];
 const dropdownInput = document.querySelector('#dropdownInput');
 const dropdownIcon = document.querySelector('#dropdownIcon');
 const dropdownWrapper = document.querySelector('#dropdownWrapper');
+const registerForm = document.querySelector('#registerForm');
+
+// Get municipalities
+const getMunicipalities = async () => {
+    try {
+        const options = {
+        method: 'GET'
+        };
+        const response = await fetch(url + '/location', options);
+        const result = await response.json();
+        // Loop result to municipalities array
+        result.forEach(item => {
+            item.municipalities.forEach(i => {
+                municipalities.push({id: i.municipality_id, name: i.municipality});
+            });
+        });
+        // Sort municipalities by name
+        municipalities.sort((a, b) => {
+            const nameA = a.name.toUpperCase(); // Ignore upper and lowercase
+            const nameB = b.name.toUpperCase(); // Ignore upper and lowercase
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            // names must be equal
+            return 0;
+        });
+        console.log(municipalities);
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+
+// Register
+registerForm.addEventListener('submit', async (e) => {
+    try {
+        e.preventDefault();
+        // Get data from form
+        const formData = new FormData(registerForm);
+        formData.set('municipality', selectedID);
+          // Create obj for json data and loop form's data to obj
+        const obj = {};
+        formData.forEach((value, key) => obj[key] = value);
+        // Create json data from obj
+        const jsonData = JSON.stringify(obj);
+        // Fetch options
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonData
+        };
+        const response = await fetch(url + '/auth/register', options);
+        const json = await response.json();
+        if(response.status !== 200) {
+            createDialog(json.message);
+            return;
+        }
+        createDialog(json.message);
+        const button = document.querySelector('dialog form button');
+        button.addEventListener('click', () => {
+            location.href = 'login.html';
+        });
+    } catch (e) {
+        console.log(e.message);
+    }
+});
 
 dropdownInput.addEventListener('input', () => {
     onInputChange(dropdownWrapper, dropdownInput, municipalities);
@@ -17,12 +87,12 @@ dropdownInput.addEventListener('focusin',  () => {
 dropdownIcon.addEventListener('click', () => {
     // Get ul list
     const listEl = document.querySelector('#dropdownList');
-
     // If it exists, remove it
     if (listEl) {
         listEl.remove();
         return;
     }
-
     addFocusToInputEl(dropdownInput);
 });
+
+getMunicipalities();
